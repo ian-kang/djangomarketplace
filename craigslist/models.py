b5351695db9336cc7c181cccd39eec261f73045c
Jared <jz3ba@virginia.edu>
2019-09-28 14:58:19 -0400

    Working Webpage, No Styling

--- craigslist/models.py
+++ craigslist/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class CreateListing(models.Model):
+    CONDITIONS = (
+        ('NEW', 'New'),
+        ('USED', 'Used'),
+        ('REFURBISHED', 'Refurbished'),
+        ('FOR_PARTS', 'For Parts or Not Working'),
+        ('OPEN_BOX', 'Open Box'),
+    )
+
+    title = models.CharField(max_length=200)
+    category = models.CharField(max_length=25, choices=CONDITIONS)
+    price = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
--- craigslist/views.py
+++ craigslist/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import CreateListing
 
 # Create your views here.
 def index(request):
     return render(request,"base.html")
+
+class CreateListingView(generic.ListView):
+    template_name = 'create_listing.html'
+    model = CreateListing
