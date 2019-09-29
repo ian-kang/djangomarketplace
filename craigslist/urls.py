c0942a23ddd694ad7da4631bbf5162c37e29f51e
Ian Kang <kang43@gmail.com>
2019-09-29 14:59:39 -0400

    nice

--- craigslist/urls.py
+++ craigslist/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('lostandfound/', views.lafView, name='laf'),
+    path('textbooks/', views.textbookView, name='textbook'),
 ]
--- craigslist/views.py
+++ craigslist/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"base.html")
+def lafView(request):
+    return render(request, "lostandfound.html")
+def textbookView(request):
+    return render(request, "textbooks.html")
