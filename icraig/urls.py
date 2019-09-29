68c48cc42fc32a7afee4c38f4310c3a3343dcdcc
stevephan98 <stevephan98@github.com>
2019-09-29 15:07:32 -0400

    finished auth

--- icraig/urls.py
+++ icraig/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', include('craigslist.urls')),
     url(r'^login/$', views.LoginView.as_view(), name='login'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
-    url(r'^auth/', include('social_django.urls', namespace='social')),
+    url(r'^auth/', include('social_django.urls', namespace='social')),  
 ]
