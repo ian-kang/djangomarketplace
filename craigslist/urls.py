af59031ed204f298f418bbade5269bfc90b27fe4
Ian Kang <kang43@gmail.com>
2019-10-10 13:44:10 -0400

    made everything one page

--- craigslist/urls.py
+++ craigslist/urls.py
@@ -5,7 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create_listing/', views.CreateListingView.as_view(success_url="/create_listing/"), name='create_listing'),
-    path('lostandfound/', views.lafView, name='laf'),
-    path('textbooks/', views.textbookView, name='textbook'),
-    path('furniture', views.furniture, name='furniture'),
+    path('s/', views.itemlist, name='itemlist'),
 ]
