12dd7472d789215cdb9e0ccebe9d7f7c72f5e1d9
Aaron Taylor <54821043+at2fb@users.noreply.github.com>
2019-10-16 14:25:26 -0400

    Debugging

--- craigslist/urls.py
+++ craigslist/urls.py
@@ -6,8 +6,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create_listing/', views.CreateListingView.as_view(success_url="/create_listing/"), name='create_listing'),
-    #path('create_listing/', views.create_listing, name='create_listing'),
+    #path('create_listing/', views.CreateListingView.as_view(success_url="/create_listing/"), name='create_listing'),
+    path('create_listing/', views.create_listing, name='create_listing'),
     path('login', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('s/', views.ItemList.as_view(), name='itemlist'),
