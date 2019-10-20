d21205850fd84a5a06731adf428cf61d66082d28
stevephan98 <stevephan98@github.com>
2019-10-20 14:49:48 -0400

    working on profile page

--- craigslist/apps.py
+++ craigslist/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class CraigslistConfig(AppConfig):
     name = 'craigslist'
+
+class UsersConfig(AppConfig):
+    name = 'users'
+
+    def ready(self):
+        import users.signals
