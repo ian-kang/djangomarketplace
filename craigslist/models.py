4b7e765d6bd9fb0a4681e3a10bddabb9ca3809e6
Aaron Taylor <at2fb@virginia.edu>
2019-11-02 19:01:12 -0400

    minor bug fixes

--- craigslist/models.py
+++ craigslist/models.py
@@ -27,7 +27,7 @@ class Listing(models.Model):
 
     title = models.CharField(max_length=200)
     acct = models.CharField(max_length=20) # Hidden field to keep track of who posted what
-    listing_id = models.CharField(max_length=50, default=str(datetime.now()).replace("-", "").replace(" ", "")) # Unique id for each listing
+    listing_id = models.CharField(max_length=50, default=str(datetime.now()).replace("-", "").replace(" ", "").replace(":", "").replace(".", "")) # Unique id for each listing
     category = models.CharField(max_length=15, choices=CATEGORIES)
     condition = models.CharField(max_length=25, choices=CONDITIONS)
     price = models.IntegerField(default=0)
