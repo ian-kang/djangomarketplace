0317a6fda8efaf012bb5e9a6e1c69878810f704a
Aaron Taylor <54821043+at2fb@users.noreply.github.com>
2019-11-07 09:26:13 -0500

    Debugging

--- craigslist/migrations/0005_listing_acct.py
+++ craigslist/migrations/0005_listing_acct.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='listing',
             name='acct',
-            field=models.CharField(default=django.utils.timezone.now, max_length=15),
+            field=models.CharField(default=django.utils.timezone.now, max_length=50),
             preserve_default=False,
         ),
     ]
