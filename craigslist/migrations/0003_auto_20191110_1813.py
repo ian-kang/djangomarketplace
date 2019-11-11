a09f348f0f98d3ae1a4b78208e2e41f990fdf7cb
Aaron Taylor <54821043+at2fb@users.noreply.github.com>
2019-11-11 00:07:15 -0500

    Update 0003_auto_20191110_1813.py

--- craigslist/migrations/0003_auto_20191110_1813.py
+++ craigslist/migrations/0003_auto_20191110_1813.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('craigslist', '0002_auto_20191110_1332'),
+        ('craigslist', '0002_auto_20191110_1414'),
     ]
 
     operations = [
