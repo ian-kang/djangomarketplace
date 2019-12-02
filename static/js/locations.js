aa9b8068519c16692d2366e3737b06e3a8094d1f
stevephan98 <stevephan98@github.com>
2019-12-02 10:18:53 -0500

    edited locations tab

--- static/js/locations.js
+++ static/js/locations.js
@@ -9,7 +9,7 @@ $("#3").on('click', function () {
 newLocation(38.033303, -78.507703);
 });
 $("#4").on('click', function () {
-newLocation(38.036357, -78.506080);
+newLocation(38.034845, -78.509317);
 });
 $("#5").on('click', function () {
 newLocation(38.0325896,-78.51063);
